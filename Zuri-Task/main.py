c5e13c34b5f0d79549b8dd524de6d626f4e485c8
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2022-06-02 15:37:31 +0100

    Revert "Update main.py"
    This reverts commit c9a1e5448e58b43a8b81347e62d5e53b8db0e831.

--- Zuri-Task/main.py
+++ Zuri-Task/main.py
@@ -3,50 +3,33 @@ import random
 msg1 = 'CONGRATS YOU WIN!'
 
 msg2 = 'YOU LOSE! TRY AGAIN'
-moves = {
-    'R': "Rock",
-    'P': "Paper",
-    'S': "Scissors",
-    }
 
 
-def decision(user_choice, computer_choice):
-    plays = [user_choice, computer_choice]
+def decision(user, computer):
+    plays = [user.upper(), computer]
+
     if plays[0] == plays[1]:
-        print_moves(user_choice, computer_choice)
         print('A TIE!')
         print('\n')
         play_again()
 
     elif plays[0] == 'R' and plays[1] == 'S':
-        print_moves(user_choice, computer_choice)
         print(msg1)
 
     elif plays[1] == 'R' and plays[0] == 'S':
-        print_moves(user_choice, computer_choice)
         print(msg2)
 
     elif plays[0] == 'P' and plays[1] == 'R':
-        print_moves(user_choice, computer_choice)
         print(msg1)
 
     elif plays[1] == 'P' and plays[0] == 'R':
-        print_moves(user_choice, computer_choice)
         print(msg2)
 
     elif plays[0] == 'S' and plays[1] == 'P':
-        print_moves(user_choice, computer_choice)
         print(msg1)
 
     elif plays[1] == 'S' and plays[0] == 'P':
-        print_moves(user_choice, computer_choice)
         print(msg2)
-
-
-def print_moves(users_move, computers_move):
-    x = moves[users_move]
-    y = moves[computers_move]
-    print(f"Player({x}) : CPU({y})")
 
 
 def game():
@@ -54,26 +37,23 @@ def game():
     print("Enter:")
     print('R for rock', 'P for paper', 'S for scissors', sep="\n")
     options = ['R', 'P', 'S']
-    user_choice = str(input('Make your choice: \n')).upper()
+    user_choice = str(input('Make your choice: \n'))
     computer_choice = random.choice(options)
-
     is_valid = False
-    x = 0
-    for items in options:
-        if user_choice == items:
-            is_valid = True
-            decision(user_choice, computer_choice)
-            break
 
-        elif user_choice != items:
-            is_valid = False
-            x += 1
+    while is_valid is False:
+        for items in options:
+            if user_choice == items:
+                is_valid = True
 
-        elif x == len(options):
-            break
+            else:
+                is_valid = False
 
-    if is_valid is False:
-        print('WRONG ENTRY!\n')
+    if is_valid is True:
+        decision(user_choice.upper(), computer_choice)
+
+    else:
+        print('Wrong entry!\n')
         game()
 
 
